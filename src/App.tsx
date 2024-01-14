f58fb99d1ef2266dc30571b8d6598c48e6bc87bb
Joel Sullivan <joel@jsullivan.us>
2024-01-13 21:02:06 -0500

    Add @mui, start new layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
-import { useState } from 'react'
-import './App.css'
-import StaticCanvas from "./canvas/StaticCanvas.tsx";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import IconButton from "@mui/material/IconButton";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import StopCircleIcon from '@mui/icons-material/StopCircle';
 import Canvas from "./canvas/Canvas.tsx";
 
-function App() {
-  const [count, setCount] = useState(0);
 
+const panelWidth = 300;
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export default function PermanentDrawerLeft() {
   return (
-    <>
-      <StaticCanvas />
-      <div style={{display: "inline-block", width: 256}} />
-      <Canvas />
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <Box sx={{ display: 'flex', width: "100vw" }}>
+        <CssBaseline />
+        <Box
+          sx={{
+            width: panelWidth,
+            minHeight: "100vh",
+            backgroundColor: "#222",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box>
+            <Divider />
+            <IconButton>
+              <StopCircleIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+              <PlayCircleIcon fontSize="large" />
+            </IconButton>
+            <Divider />
+          </Box>
+        </Box>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Canvas />
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
-
-export default App;
